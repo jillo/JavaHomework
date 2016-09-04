64b0359fa2608f349b2a615726be8d131580386d
Jill <ott@csdisco.com>
2016-09-04 10:32:50 -0500

    initial commit of Assignment3

---
+++ Assignment3/src/com/csdisco/qa/Bicycle.java
@@ -0,0 +1,65 @@
+package com.csdisco.qa;
+
+class Bicycle {
+
+    int cadence = 0;
+    int gear = 1;
+    float speed = 0;
+
+    void changeCadence(int newValue) {      //changecadence = 50
+        if (newValue < 0) {
+            cadence = 0;
+        }
+        else if (newValue > 100){
+            cadence = 100;
+        }
+
+        else {
+            cadence = newValue;
+        }
+
+        updateSpeed();
+    }
+
+    void changeGear(int newValue) {
+
+        if (newValue < 1){
+            gear = 1;
+        }
+
+        else if (newValue > 6){
+            gear = 6;
+        }
+
+        else {
+            gear = newValue;
+        }
+
+        updateSpeed();
+    }
+
+    void updateSpeed (){
+
+        int inchesPerMin;
+        float milesPerHour;
+        int tireCircumference = 80;
+        int tireRPM = 0;
+        int inchesPerMile = 63360; // there are 63360 inches in a mile
+
+        tireRPM = gear * cadence;
+        inchesPerMin = tireRPM * tireCircumference;
+        milesPerHour = (float) inchesPerMin/inchesPerMile;
+        speed = milesPerHour;
+
+    }
+
+    void speedUp(int increment) { cadence = cadence + increment; }
+
+    void applyBrakes(int decrement) {cadence = cadence - decrement; }
+
+    void printStates() {
+         System.out.println("cadence:" + cadence
+                 + " speed:" + speed
+                 + " gear:" + gear);
+    }
+}
---
+++ Assignment3/src/com/csdisco/qa/VariableWheelBicycle.java
@@ -0,0 +1,7 @@
+package com.csdisco.qa;
+
+/**
+ * Created by jillott on 9/3/16.
+ */
+public class VariableWheelBicycle extends Bicycle {
+}
